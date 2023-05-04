9c3dcf431861fdc1f38f8603bcf9bd2b9f9ef40b
Gesley de Oliveira Rosa <geeh.oliveira@hotmail.com>
2023-05-04 12:20:17 -0300

    exercicio

---
+++ Aula9/exercicio02.py
@@ -0,0 +1,16 @@
+lista = []
+numero = 0
+
+
+
+while True:
+    n = int(input("Entre com um vetor de 10 elementos numéricos: "))
+    lista
+    if len(n) == 10:
+        break
+    print("O vetor incorreto!")
+
+for i in n:
+    if i > numero:
+        numero = i
+print(f"O maior número da lista de 10 elementos é {numero}")
