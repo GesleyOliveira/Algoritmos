cd34dda4c0ae700283cb4ac0261668c07d5fd746
Gesley de Oliveira Rosa <geeh.oliveira@hotmail.com>
2023-05-04 17:46:49 -0300

    exercicio

--- Aula9/exercicio02.py
+++ Aula9/exercicio02.py
@@ -1,4 +1,15 @@
 vetor = []
-for i in range(1):
-    vetor.append(int(input("Entre com o valor numérico: ")))
-print(vetor)
+
+for i in range(1, 11):
+    valor = int(input(f"Digite o {i}º valor: "))
+    vetor.append(valor)
+
+maior_valor = vetor[0]
+posicao_maior_valor = 0
+
+for i in range(1, 10):
+    if vetor[i] > maior_valor:
+        maior_valor = vetor[i]
+        posicao_maior_valor = i
+
+print("O maior valor é {maior_valor} e está na posição {posicao_maior_valor}")
